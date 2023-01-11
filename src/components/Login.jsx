6bf40222c0519d8548f7dee8bb970c375a3a148e
Augustine Ugwu <105177548+augustine-ugwu@users.noreply.github.com>
2023-01-11 07:30:46 +0100

    Created form and added darkmode theme

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Button, Flex, Heading, Input, useColorMode, useColorModeValue } from '@chakra-ui/react'
 
 const Login = () => {
+const {toggleColorMode} = useColorMode()
+const formBackground = useColorModeValue("gray.100", "gray.700")
   return (
-    <div>Login</div>
+    
+    <Flex height={"100vh"} alignItems={"center"} justifyContent={"center"}>
+      <Flex direction={"column"} background={formBackground} p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+        <Input placeholder="Enter email" variant="filled" mb={3} type="email" />
+        <Input placeholder='**********' variant={"filled"} mb={6} type="password" />
+        <Button mb={6} colorScheme={"teal"}>Log in</Button>
+        <Button onClick={toggleColorMode}>Toggle Colour Mode</Button>
+      </Flex>
+    </Flex>
   )
 }
 
